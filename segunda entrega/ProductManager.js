acc36d6760ef9c79d249c3b5ded86ad8d2dd6cec
julio6543 <juliogonzalosanchez21@gmail.com>
2024-02-24 16:12:50 -0300

    nuevos-cambios

--- segunda entrega/ProductManager.js
+++ segunda entrega/ProductManager.js
@@ -1,6 +1,25 @@
 class ProductManager {
     constructor() {
-        this.products = [];
+        this.products = [
+            {
+                "id": "_ncf1asmz9",
+                "title": "Laptop ",
+                "description": "Laptop DELL.",
+                "price": 1200,
+                "thumbnail": "laptop.jpg",
+                "code": "hp001",
+                "stock": 10
+            },
+            {
+                "id": "_kc7z3tjss",
+                "title": "PC Gamer Red Dragon",
+                "description": "Potente PC de escritorio diseñado para juegos de alta gama con Ryzen 9 y RTX 3080.",
+                "price": 2500,
+                "thumbnail": "pc_gamer.jpg",
+                "code": "awr10",
+                "stock": 5
+            }
+        ];
     }
 
     getProducts() {
@@ -35,7 +54,6 @@ class ProductManager {
             throw new Error('Producto no encontrado');
         }
 
-
         this.products[productIndex] = { ...this.products[productIndex], ...updatedFields };
 
         return this.products[productIndex];
@@ -47,41 +65,41 @@ class ProductManager {
             throw new Error('Producto no encontrado');
         }
 
-
         this.products.splice(productIndex, 1);
     }
 }
 
-
 const productManager = new ProductManager();
 
-
+console.log("Lista de productos inicial:");
 console.log(productManager.getProducts());
-
-
-productManager.addProduct("producto ", "Este es un producto ", 200, "Sin imagen", "jk94", 25);
-
-
-console.log(productManager.getProducts());
-
 
 try {
+    productManager.addProduct("Nuevo Producto", "Descripción del nuevo producto", 999, "new_product.jpg", "new001", 20);
+    console.log("Lista de productos después de agregar un nuevo producto:");
+    console.log(productManager.getProducts());
+} catch (error) {
+    console.error(error.message);
+}
+
+try {
+    console.log("Obtener el producto por ID:");
     console.log(productManager.getProductById(productManager.getProducts()[0].id));
 } catch (error) {
     console.error('¡Ups, ocurrió un error al obtener el producto por ID!');
 }
 
-
 try {
     productManager.updateProduct(productManager.getProducts()[0].id, { price: 250 });
+    console.log("Lista de productos después de actualizar el precio:");
     console.log(productManager.getProducts());
 } catch (error) {
     console.error('¡Ups, ocurrió un error al actualizar el producto!');
 }
 
-
 try {
     productManager.deleteProduct(productManager.getProducts()[0].id);
+    console.log("Lista de productos después de eliminar un producto:");
     console.log(productManager.getProducts());
 } catch (error) {
     console.error('¡Ups, ocurrió un error al eliminar el producto!');
